1b17658da33628d1b8765d5be0bc3427c50fa5aa
Amara002 <amaraalbalkhi@yahoo.com>
2021-06-27 23:09:26 +0300

    finish code challenge 01

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        int arr[] = {1,2,3,4,5};
+        Stack <Integer> newArr = new Stack <Integer>();
+        for (int i=arr.length-1 ; i<arr.length ; i--){
+            newArr.push(arr[i]);
+            if(i == 0){
+                break;
+            }
+
+        }
+        System.out.println(newArr);
     }
 }
+
