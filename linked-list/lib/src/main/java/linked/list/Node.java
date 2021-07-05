a2df2b4097672f190b6c803b532307c9917c998d
Amara002 <amaraalbalkhi@yahoo.com>
2021-07-05 22:43:11 +0300

    finish codeChallenge 05

---
+++ linked-list/lib/src/main/java/linked/list/Node.java
@@ -0,0 +1,20 @@
+package linked.list;
+
+public class Node {
+    public Node next;
+    private int data;
+//    private Node next;
+
+    public Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+}
