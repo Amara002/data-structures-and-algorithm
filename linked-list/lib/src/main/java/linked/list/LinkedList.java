dbe73768d59adbcc0f86f7f1231c394be5a996fc
Amara002 <amaraalbalkhi@yahoo.com>
2021-07-13 22:58:14 +0300

    finish codeChallenge 06

--- linked-list/lib/src/main/java/linked/list/LinkedList.java
+++ linked-list/lib/src/main/java/linked/list/LinkedList.java
@@ -1,47 +1,91 @@
 package linked.list;
-
-public class LinkedList {
- Node head;
-
-    public LinkedList(Node head) {
-        this.head = head;
+public class LinkedList <T>{
+    public Node<T>  head;
+    public int length;
+    public LinkedList() {
+        this.head = null;
     }
-    public void insert(Node newNode){
-        newNode.next = head;
-        head = newNode;
-    }
-
-    public void show(){
-        Node firstNode = head;
-        while(firstNode != null){
-            System.out.println(firstNode.getData());
-            firstNode = firstNode.next;
+    public void insert(T data){
+        Node<T> newNode = new Node<>(data);
+        if(this.head == null) {
+            head = newNode;
         }
-    }
-    public boolean include(Node head,int val){
-    Node firstNode = head;
-    while( firstNode != null){
-        if(firstNode.getData() == val){
-            return true;
+        else {
+            Node temp = head;
+            head = newNode;
+            head.next = temp;
         }
-        firstNode = firstNode.next;
+        length++;
     }
-    return false;
+    public void insertAfter(T data ,T newData){
+        Node<T> newNode = new Node<>(newData);
+        if(this.head == null) {
+            head = newNode;
+        }
+        else {
+            Node<T> temp = head;
+            while (temp.data != data) {
+                temp = temp.next;
+            }
+            newNode.next = temp.next;
+            temp.next = newNode;
+        }
+        length++;
+    }
+    public void insertBefore(T data , T newData){
+        Node<T> newNode = new Node<>(newData);
+        if(this.head == null) {
+            head = newNode;
+        }else if(data == head.data){
+            newNode.next = head;
+            head = newNode;
+        }
+        else {
+            Node<T> temp = head;
+            while (temp.next.data != data) {
+                temp = temp.next;
+            }
+            newNode.next = temp.next;
+            temp.next = newNode;
+        }
+        length++;
+    }
+    public void append (T data) {
+        Node<T> newNode = new Node<>(data);
+        if(this.head == null) {
+            head = newNode;
+        }
+        else {
+            Node<T> temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+        length++;
+    }
+    public boolean includes (T key) {
+        Node<T> temp = head;
+        while (temp != null) {
+            if(temp.data.equals(key)){
+                return true;
+            }
+            temp = temp.next;
+        }
+        return false;
     }
 
     @Override
     public String toString() {
-        String res = " ";
-        Node newNod = head;
-        while (newNod != null){
-            res+="{";
-            res += newNod.getData();
-            res+="}";
-            res+="->";
-            newNod = newNod.next;
+        String result = "{ ";
+        Node<T> temp = head;
+        while (temp != null) {
+            result += temp.data + " -> ";
+            temp = temp.next;
         }
-        return "LinkedList" +
-                "linkedList=" + res + "Null"
-                ;
+        if (temp == null) {
+            result += temp + " }";
+        }
+        return result;
     }
 }
--- linked-list/lib/src/main/java/linked/list/Node.java
+++ linked-list/lib/src/main/java/linked/list/Node.java
@@ -1,20 +1,11 @@
 package linked.list;
 
-public class Node {
+public class Node<T> {
     public Node next;
-    private int data;
-//    private Node next;
-
-    public Node(int data, Node next) {
+    public T data;
+    public Node( T data) {
         this.data = data;
-        this.next = next;
-    }
-
-    public int getData() {
-        return data;
-    }
-
-    public void setData(int data) {
-        this.data = data;
+        this.next = null;
     }
 }
+
